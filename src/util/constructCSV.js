c726b05c17c71487351ead6ad10e6f7af044e9d5
Shane Keller <Shane@Shanes-MacBook-Pro.local>
2015-02-22 14:02:16 -0800

    Add bluebird promise library. Clean up constructCSV logic.
    Still need to separate database and csv-parsing logic, as well as refactor to use promises.

--- src/util/constructCSV.js
+++ src/util/constructCSV.js
@@ -4,6 +4,7 @@ var MongoClient = require('mongodb').MongoClient;
 var mongoose = require('mongoose');
 var parse = require('csv-parse');
 
+var promise = require('./promise');
 var FoodTruck = require('../models/foodtruck');
 
 var CONNECTION_STRING = require('./dbHelpers').CONNECTION_STRING;
@@ -27,61 +28,7 @@ var endpointOptions = {
 var csvText = '';
 
 http.get(endpointOptions, function(res) {
-    res.on('data', function(chunk) {
-        csvText += chunk;
-    });
-
-    res.on('error', function(e) {
-        console.log('error fetching data', e.message);
-    });
-
-    res.on('end', function() {
-        endpointOptions.path = FOOD_TRUCK_PATH + queryString2;
-
-        http.get(endpointOptions, function(res) {
-            res.on('data', function(chunk) {
-                csvText += chunk;
-            });
-
-            res.on('error', function(e) {
-                console.log('error fetching data', e.message);
-            });
-
-            res.on('end', function() {
-                parse(csvText, function(err, foodTrucks) {
-                    if (err) {
-                        res.statusCode = 400;
-                        // res.send('error parsing csv: ' + err.message);
-                    }
-
-                    var foodTruckDocs = convertDataToDocs(foodTrucks);
-
-                    console.log(foodTrucks.length, foodTruckDocs.length);
-
-                    // mongoose.connect(CONNECTION_STRING);
-
-                    // var db = mongoose.connection;
-
-                    // // If the Node process ends, close the Mongoose connection
-                    // process.on('SIGINT', function() {
-                    //     mongoose.connection.close(function () {
-                    //         console.log('Mongoose default connection disconnected through app termination');
-                    //         process.exit(0);
-                    //     });
-                    // });
-
-                    // db.once('open', function() {
-                    //     createFoodTrucks(foodTruckDocs)
-                    //     .then(function() {
-                    //         mongoose.disconnect();
-                    //     });
-                    // });
-
-                    // db.on('error', console.error.bind(console, 'connection error:'));
-                });
-            });
-        });
-    });
+    addToCSV(res, getRemainingCSVChunks);
 });
 
 function addToCSV(res, callback) {
@@ -95,7 +42,41 @@ function addToCSV(res, callback) {
 
     res.on('end', function() {
         callback(res);
-    })
+    });
+}
+
+function addCSVToDatabase(res) {
+    parse(csvText, function(err, foodTrucks) {
+        if (err) {
+            res.statusCode = 400;
+            // res.send('error parsing csv: ' + err.message);
+        }
+
+        var foodTruckDocs = convertDataToDocs(foodTrucks);
+
+        console.log(foodTrucks.length, foodTruckDocs.length);
+
+        // mongoose.connect(CONNECTION_STRING);
+
+        // var db = mongoose.connection;
+
+        // // If the Node process ends, close the Mongoose connection
+        // process.on('SIGINT', function() {
+        //     mongoose.connection.close(function () {
+        //         console.log('Mongoose default connection disconnected through app termination');
+        //         process.exit(0);
+        //     });
+        // });
+
+        // db.once('open', function() {
+        //     createFoodTrucks(foodTruckDocs)
+        //     .then(function() {
+        //         mongoose.disconnect();
+        //     });
+        // });
+
+        // db.on('error', console.error.bind(console, 'connection error:'));
+    });
 }
 
 // Given an array of objects, maps the objects to the foodTruckSchema in
@@ -118,5 +99,13 @@ function createFoodTrucks(foodTrucks) {
         }
         var created = Array.prototype.slice.call(arguments, 1);
         console.log('Added', created.length, 'food trucks.');
+    });
+}
+
+function getRemainingCSVChunks(res) {
+    endpointOptions.path = FOOD_TRUCK_PATH + queryString2;
+
+    http.get(endpointOptions, function(res) {
+        addToCSV(res, addCSVToDatabase);
     });
 }
