85199776aa999e0ffe7b6fbe3ba6ee89adc9fe1a
Shane Keller <Shane@Shanes-MacBook-Pro.local>
2015-03-08 13:36:17 -0700

    Remove unneeded .js files in test/

--- app/config/db.py
+++ app/config/db.py
@@ -7,7 +7,7 @@ class DB:
     connection_string = os.environ.get('MONGOHQ_URL',
                                        'mongodb://127.0.0.1:27017/foodtrucks')
     # TODO(shane): don't hard code production database name
-    database_name = 'app34699712' if os.environ.get('MONGOHQ_URL') \
+    database_name = 'app34296442' if os.environ.get('MONGOHQ_URL') \
         else 'foodtrucks'
     connection = MongoClient(connection_string)
     db = connection[database_name]
