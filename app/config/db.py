b7c306951f7d1cfab28a28a968067ed704a1dc04
Shane Keller <Shane@Shanes-MacBook-Pro.local>
2015-03-08 11:49:49 -0700

    Use either local or production database in config/db.py

--- app/config/db.py
+++ app/config/db.py
@@ -1,7 +1,11 @@
+import os
+
 from pymongo import MongoClient
 
 
 class DB:
-    connection = MongoClient('localhost', 27017)
+    connection_string = os.environ.get('MONGOHQ_URL',
+                                       'mongodb://127.0.0.1:27017')
+    connection = MongoClient(connection_string)
     db = connection['foodtrucks']
     food_trucks = db.foodtrucks
