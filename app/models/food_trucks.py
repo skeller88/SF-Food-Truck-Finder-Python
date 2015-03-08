2f13c5bfa6ec9a914103b4d6d96d36b77fefd4db
Shane Keller <Shane@Shanes-MacBook-Pro.local>
2015-03-07 17:28:07 -0800

    find_nearest()

--- app/models/food_trucks.py
+++ app/models/food_trucks.py
@@ -19,13 +19,52 @@ def find_nearest(options):
     # TODO(shane): does this pattern work in python too?
     limit = options['limit'] or 10
     within = (options['within'] if options['within'] else options['within'] * meters_per_mile)
-    return 4
+
+    food_trucks.ensure_index({'location': '2dsphere'})
+    food_trucks.aggregate([
+        {
+            '$geoNear': {
+                'distance_field': 'distance',
+                # This query must have the property 'spherical: true'.
+                # Because of that, the distance is returned in meters and
+                # needs to be converted to miles.
+                'distance_multiplier': 1/meters_per_mile,
+                'max_distance': within,
+                'near': {
+                    'type': 'Point',
+                    'coordinates': options.coordinates
+                },
+                'spherical': True,
+            },
+        },
+        {
+            '$limit': limit
+        },
+            # $sort must be before $project to take advantage of indexes:
+            # docs.mongodb.org/manual/reference/operator/aggregation/sort/
+        {
+            '$sort': {
+                'distance': 1,
+                'name': 1
+            }
+        },
+        {
+            '$project': {
+                '_id': 0,
+                'address': 1,
+                'distance': 1,
+                'fooditems': 1,
+                'location': 1,
+                'name': 1
+            }
+        },
+    ])
 
 def update_food_trucks(updated):
+    original = len(list(food_trucks.find()))
     food_trucks.drop()
-    print 'Removed existing food trucks.'
+    print 'Removed', original, 'existing food trucks.'
 
     food_trucks.insert(updated)
-    results = list(food_trucks.find())
-
-    print 'Replaced with', len(results), 'food trucks.'
+    new = list(food_trucks.find())
+    print 'Replaced with', len(new), 'food trucks.'
