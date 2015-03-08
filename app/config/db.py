1306343b71a6d183f49ca5352c0c36428a84b479
Shane Keller <Shane@Shanes-MacBook-Pro.local>
2015-03-08 12:54:14 -0700

    Try fixing production database connection error.
    The error is:
    `pymongo.errors.OperationFailure: database error: not authorized for query on foodtrucks.foodtrucks`
    
    The error seems to be because the production database name is not 'foodtrucks'. So I figured out the production database name from the connection string and hard coded it into config/db.

--- app/config/db.py
+++ app/config/db.py
@@ -5,7 +5,10 @@ from pymongo import MongoClient
 
 class DB:
     connection_string = os.environ.get('MONGOHQ_URL',
-                                       'mongodb://127.0.0.1:27017')
+                                       'mongodb://127.0.0.1:27017/foodtrucks')
+    # TODO(shane): don't hard code production database name
+    database_name = 'app34699712' if os.environ.get('MONGOHQ_URL') \
+        else 'foodtrucks'
     connection = MongoClient(connection_string)
-    db = connection['foodtrucks']
+    db = connection[database_name]
     food_trucks = db.foodtrucks
